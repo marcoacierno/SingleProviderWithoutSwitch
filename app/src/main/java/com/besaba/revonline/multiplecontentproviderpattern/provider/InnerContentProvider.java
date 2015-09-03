fd720307236c2b477836e0192403ba4fd957c395
Marco Acierno <marcoaciernoemail@gmail.com>
2015-09-03 15:26:10 +0200

    Add documentation to getUris and getProviderName

--- app/src/main/java/com/besaba/revonline/multiplecontentproviderpattern/provider/InnerContentProvider.java
+++ app/src/main/java/com/besaba/revonline/multiplecontentproviderpattern/provider/InnerContentProvider.java
@@ -7,10 +7,21 @@ import android.net.Uri;
 import java.util.Map;
 
 public interface InnerContentProvider {
+  /**
+   * The name of the provider. It will be used to generate the URI.
+   * For example, if you return "books" your uris will start at
+   * books/
+   */
   String getProviderName();
 
+  /**
+   * <p>Use a map implementation that keep insertion-order! Otherwise
+   * the UriMatcher could fail to match your uris.</p>
+   * 
+   * @returns a Map which contains the uris of the provider and the ids assigned to them
+   */
   Map<String, Integer> getUris();
-
+  
   Cursor query(final int matchId, final String[] projection, final String selection, final String[] selectionArgs, final String sortOrder);
   Uri insert(final int matchId, final ContentValues values);
   int delete(final int matchId, final String selection, final String[] selectionArgs);
